e1b5633b6a6b8b794ff7f7d1be06a573cb434343
Nadezda Ermakova <nadezda.epifanova@gmail.com>
2023-12-14 15:23:32 +0100

    add homework10 get a set

---
+++ HomeWork/Domácí úkoly - 10. lekce - get a set/Osoba.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domácí_úkoly___10._lekce___get_a_set;
+
+internal class Osoba
+{
+    private string _jmeno;
+    private string _prijmeni;
+    private int _vek;
+    private bool _jePlnoleta;
+    public string Jmeno 
+    {
+        get { return _jmeno; }
+        set { _jmeno = value; }
+    }
+    public string Prijmeni
+    {
+        get { return _prijmeni; }
+        set { _prijmeni = value; }
+    }
+    public int Vek 
+    {
+        get 
+        { 
+            return _vek; 
+        }
+        set 
+        {  
+            if (_vek >= 0 && _vek<=100) 
+            {
+                _vek = value;
+                if (_vek >= 18)
+                {
+                    _jePlnoleta = true;
+                }
+                else
+                {
+                    _jePlnoleta = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vek je zadan spatne");
+            }
+        }
+    }
+    public bool JePlnoleta { get { return _jePlnoleta; } }
+}
